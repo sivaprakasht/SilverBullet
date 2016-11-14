b991f0dc8b314121912e91898720229ac581aec0
sthiyag <sivaprakash_thiyagarajan@trimble.com>
2016-11-14 20:37:36 +0530

    Added 3 Charts for inflow meter

--- js/measurementGraph.js
+++ js/measurementGraph.js
@@ -5,12 +5,11 @@ var measurementTypes = {};
 var siteMeasurementsObj = {};
 var measurementsUnitsObj = {};
 
-var rainGauges = ["RG01","RG02","RG02"];
-var inFlowMeters = ["T03-005","T03-005","T03-005"];
+var rainGauges = ["RG01","RG02","RG02","RG01","RG02"];
+var inFlowMeters = ["T03-005","T03-005","T03-005","T03-005"];
 var sites = ["RG01","RG02","RG02","T03-005"];
 
 var sitesData= [];
-
 var Trimble = {};
 Trimble.baseUrl = "https://goldenberyl.trimbleunity.com/unity";
 Trimble.token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE0Nzg5MzEwNjUsImV4cCI6MTUxMDQ2NzA3NywiYXVkIjoiIiwic3ViIjoiIiwidXNlck5hbWUiOiJsb2tlc2giLCJ0ZW5hbnQiOiJkZXYxNjIifQ.7yblAQPT8vBP2oARNR_gDbjutBC1hJrewTHUvRSHtwY";
@@ -26,9 +25,8 @@ $(document).ready(function () {
     loadSiteDataFromTelog();
     renderRainGaugeCharts();
     renderInflowCharts();
+    drawStorageChart();
 });
-
-
 
 function loadSiteDataFromTelog(){
     var defaultStartDate = new Date("2016-07-29T04:00:00");
@@ -192,8 +190,6 @@ function getRainFallDataAndDate(){
     rainFallData.data = seriesRainFallData;
     return rainFallData;
 }
-
-
 
 function renderRainGaugeCharts(){
     var rainFallData = getRainFallDataAndDate();
@@ -372,7 +368,122 @@ function drawInflowChart(containerIndex, data){
             point.update(newVal);
         }
     }, 2000);
+}
 
+function drawStorageChart(){
+
+    Highcharts.chart('storageContainer', {
+
+            chart: {
+                type: 'gauge',
+                plotBackgroundColor: null,
+                plotBackgroundImage: null,
+                plotBorderWidth: 0,
+                plotShadow: false
+            },
+
+            title: {
+                text: 'RESERVOIR CAPACITY'
+            },
+
+            pane: {
+                startAngle: -150,
+                endAngle: 150,
+                background: [{
+                    backgroundColor: {
+                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                        stops: [
+                            [0, '#FFF'],
+                            [1, '#333']
+                        ]
+                    },
+                    borderWidth: 0,
+                    outerRadius: '109%'
+                }, {
+                    backgroundColor: {
+                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                        stops: [
+                            [0, '#333'],
+                            [1, '#FFF']
+                        ]
+                    },
+                    borderWidth: 1,
+                    outerRadius: '107%'
+                }, {
+                    // default background
+                }, {
+                    backgroundColor: '#DDD',
+                    borderWidth: 0,
+                    outerRadius: '105%',
+                    innerRadius: '103%'
+                }]
+            },
+
+            // the value axis
+            yAxis: {
+                min: 0,
+                max: 100,
+
+                minorTickInterval: 'auto',
+                minorTickWidth: 1,
+                minorTickLength: 10,
+                minorTickPosition: 'inside',
+                minorTickColor: '#666',
+
+                tickPixelInterval: 30,
+                tickWidth: 2,
+                tickPosition: 'inside',
+                tickLength: 10,
+                tickColor: '#666',
+                labels: {
+                    step: 2,
+                    rotation: 'auto'
+                },
+                title: {
+                    text: 'Feet'
+                },
+                plotBands: [{
+                    from: 0,
+                    to: 60,
+                    color: '#55BF3B' // green
+                }, {
+                    from: 60,
+                    to: 90,
+                    color: '#DDDF0D' // yellow
+                }, {
+                    from: 90,
+                    to: 100,
+                    color: '#DF5353' // red
+                }]
+            },
+
+            series: [{
+                name: 'ReservoirCapacity',
+                data: [80],
+                tooltip: {
+                    valueSuffix: ' Feet'
+                }
+            }]
+
+        },
+        // Add some life
+        function (chart) {
+            if (!chart.renderer.forExport) {
+                setInterval(function () {
+                    var point = chart.series[0].points[0],
+                        newVal,
+                        inc = Math.round((Math.random() - 0.5) * 20);
+
+                    newVal = point.y + inc;
+                    if (newVal < 0 || newVal > 100) {
+                        newVal = point.y - inc;
+                    }
+
+                    point.update(newVal);
+
+                }, 3000);
+            }
+        });
 }
 
 /*function getRainFallDataAndDate2(){
